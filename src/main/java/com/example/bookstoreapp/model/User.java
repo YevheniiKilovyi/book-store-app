3f010470b210eafbb96f62d4eafa201d824f2ca0
Yevhenii Kilovyi <118058456+YevheniiKilovyi@users.noreply.github.com>
2023-08-18 13:32:18 +0300

    feat(books): implemented User entity and Role entity

---
+++ src/main/java/com/example/bookstoreapp/model/User.java
@@ -0,0 +1,56 @@
+package com.example.bookstoreapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.util.Set;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.commons.lang3.builder.ToStringExclude;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @NotNull
+    @Column(unique = true,
+            name = "email")
+    private String email;
+    @NotNull
+    @Column(name = "password")
+    @ToStringExclude
+    private String password;
+    @NotNull
+    @Column(name = "first_name")
+    private String firstName;
+    @NotNull
+    @Column(name = "last_name")
+    private String lastName;
+    @NotNull
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
